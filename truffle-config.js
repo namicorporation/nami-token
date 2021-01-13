57a99b6374c5e45dac143c34bfff02d05e2e5100
Hiep Tran <hiepth@nami.trade>
2021-01-13 15:49:20 +0700

    Fix for audit

--- truffle-config.js
+++ truffle-config.js
@@ -66,6 +66,14 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
+    development: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC_PHRASE_DEV, process.env.JSON_RPC_DEV),
+      network_id: '*',       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC_PHRASE_TEST, process.env.ROPSTEN_JSON_RPC),
       network_id: 3,       // Ropsten's id
